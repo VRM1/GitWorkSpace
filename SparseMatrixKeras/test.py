05a4ccfb62003254fd093513621d4d27ea81475a
Rakesh <vineethrakesh@wayne.edu>
2018-07-03 08:26:19 -0700

    first commit

---
+++ SparseMatrixKeras/test.py
@@ -0,0 +1,74 @@
+from __future__ import print_function
+import numpy as np
+np.random.seed(1337)  # for reproducibility
+
+from keras.datasets import reuters
+from keras.models import Sequential
+from keras.layers import Dense, Dropout, Activation
+from keras.utils import np_utils
+from keras.preprocessing.text import Tokenizer
+from scipy import sparse
+
+max_words = 1000
+batch_size = 32
+nb_epoch = 5
+
+print('Loading data...')
+(X_train, y_train), (X_test, y_test) = reuters.load_data(nb_words=max_words, test_split=0.2)
+print(len(X_train), 'train sequences')
+print(len(X_test), 'test sequences')
+
+nb_classes = np.max(y_train)+1
+print(nb_classes, 'classes')
+
+print('Vectorizing sequence data...')
+tokenizer = Tokenizer(nb_words=max_words)
+X_train = tokenizer.sequences_to_matrix(X_train, mode='binary')
+X_test = tokenizer.sequences_to_matrix(X_test, mode='binary')
+print('X_train shape:', X_train.shape)
+print('X_test shape:', X_test.shape)
+
+print('Convert class vector to binary class matrix (for use with categorical_crossentropy)')
+Y_train = np_utils.to_categorical(y_train, nb_classes)
+Y_test = np_utils.to_categorical(y_test, nb_classes)
+print('Y_train shape:', Y_train.shape)
+print('Y_test shape:', Y_test.shape)
+
+X_train_sparse = sparse.csr_matrix(X_train)
+
+def batch_generator(X, y, batch_size, shuffle):
+    number_of_batches = X.shape[0]/batch_size
+    counter = 0
+    sample_index = np.arange(X.shape[0])
+    if shuffle:
+        np.random.shuffle(sample_index)
+    while True:
+        batch_index = sample_index[batch_size*counter:batch_size*(counter+1)]
+        X_batch = X[batch_index,:].toarray()
+        y_batch = y[batch_index]
+        counter += 1
+        yield X_batch, y_batch
+        if (counter == number_of_batches):
+            if shuffle:
+                np.random.shuffle(sample_index)
+            counter = 0
+
+print('Building model...')
+model = Sequential()
+model.add(Dense(512, input_shape=(max_words,)))
+model.add(Activation('relu'))
+model.add(Dropout(0.5))
+model.add(Dense(nb_classes))
+model.add(Activation('softmax'))
+
+
+model.compile(loss='categorical_crossentropy',
+          optimizer='adam',
+          metrics=['accuracy'])
+
+history = model.fit_generator(generator=batch_generator(X_train_sparse, Y_train, 32, True),
+                              samples_per_epoch=X_train_sparse.shape[0],nb_epoch=nb_epoch,
+                                verbose=1)
+score = model.evaluate(X_test, Y_test, batch_size=batch_size, verbose=1)
+print('Test score:', score[0])
+print('Test accuracy:', score[1])
